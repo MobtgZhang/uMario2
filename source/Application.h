d2f933e3c86f1927383862ab9b8bf0f47bbcd7a0
mobtgzhang <mobtgzhang@outlook.com>
2023-01-28 14:57:50 +0800

    update codes

---
+++ source/Application.h
@@ -0,0 +1,10 @@
+# ifndef APPLICATION_HEADERS
+# define APPLICATION_HEADERS
+class Application{
+public:
+    Application();
+    ~Application();
+    void mainloop();
+};
+
+# endif
---
+++ source/main.cpp
@@ -0,0 +1,8 @@
+# include "Application.h"
+# include <cstdio>
+int main(){
+    Application app;
+    app.mainloop();
+    printf("Here is the end of program!\n");
+    return 0;
+}
